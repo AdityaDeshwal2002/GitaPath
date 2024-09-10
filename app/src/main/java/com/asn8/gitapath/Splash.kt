ab35da5f045647a01d0d5c0bf303b2f51d4fc1fc
AdityaDeshwal2002 <adityadeshwal02@gamil.com>
2024-09-11 02:20:33 +0530

    SplashFragment to HomeFragment code

--- app/src/main/java/com/asn8/gitapath/Splash.kt
+++ app/src/main/java/com/asn8/gitapath/Splash.kt
@@ -1,11 +1,14 @@
 package com.asn8.gitapath
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
 class Splash : Fragment() {
     override fun onCreateView(
@@ -18,6 +21,7 @@ class Splash : Fragment() {
 
         val loadingImage: ImageView = view.findViewById(R.id.loadingSplashScreen);
         loadingImage.setImageResource(R.drawable.splashLoading)
+        Handler(Looper.getMainLooper()).postDelayed({findNavController().navigate(R.id.action_splash_to_home2)},5000)
         return view;
     }
 }
